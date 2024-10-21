8c1ad48c961eb73b1511eff17466d348995a4e73
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-21 08:36:44 +0000

    Setting up macho man

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Generator from "./components/Generator"
+import Hero from "./components/Hero"
 
 
 function App() {
   
   return (
     <main>
-      
+      <Hero/>
+      <Generator/>
     </main>
   )
 }
