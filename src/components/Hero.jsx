abc98e78da4d9a5f06086d2a3b2ad4e55c9b43e7
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-25 08:09:07 +0000

    Working on the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 
 export default function Hero() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col fa-align-center justify-center">
       <p>IT&apos;S TIME TO BE A</p>
       <h1>Macho<span>Man</span></h1>
       <p>I hereby acknowledgement that I may become <span>unbelievably swolenormous</span> and accept all risks of becoming the local <span>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
