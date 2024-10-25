1d19f67cb5ae26975abd65bfe8041e6860aeba41
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-25 08:03:25 +0000

    Working on the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ export default function Hero() {
     <div>
       <p>IT&apos;S TIME TO BE A</p>
       <h1>Macho<span>Man</span></h1>
+      <p>I hereby acknowledgement that I may become</p>
     </div>
   )
 }
