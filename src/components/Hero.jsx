a1504e218cf4c032018c4e3f86b57d141b21bf65
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-28 08:55:06 +0000

    Working on the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
       <p>IT&apos;S TIME TO BE A</p>
       <h1 className="uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Macho<span className="text-blue-400">Man</span></h1>
       </div>
-      <p className="text-sm md:text-base font-light">I hereby acknowledgement that I may become <span>unbelievably swolenormous</span> and accept all risks of becoming the local <span>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+      <p className="text-sm md:text-base font-light">I hereby acknowledgement that I may become <span className="text-blue-400 font-medium">unbelievably swolenormous</span> and accept all risks of becoming the local <span className="text-blue-400 font font-medium">mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
     </div>
   )
 }
