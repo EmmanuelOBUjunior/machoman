32d3d16cc1acac5ba517ca011e3bb58a131c161d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-28 09:20:29 +0000

    Working on the hero button Accept and Begin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 
 export default function Hero() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center text-center">
+    <div className="min-h-screen flex flex-col items-center justify-center text-center max-w-[800px]">
       <div className="flex flex-col gap-4">
       <p>IT&apos;S TIME TO BE A</p>
       <h1 className="uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Macho<span className="text-blue-400">Man</span></h1>
