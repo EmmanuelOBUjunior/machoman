cb911b6c0b9d0264cf7ef9cf1d27462776837674
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-28 08:53:37 +0000

    Working on the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,11 @@
 export default function Hero() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="flex flex-col gap-4"></div>
+      <div className="flex flex-col gap-4">
       <p>IT&apos;S TIME TO BE A</p>
-      <h1 className="uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Macho<span>Man</span></h1>
-      <p className="">I hereby acknowledgement that I may become <span>unbelievably swolenormous</span> and accept all risks of becoming the local <span>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+      <h1 className="uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Macho<span className="text-blue-400">Man</span></h1>
+      </div>
+      <p className="text-blue-400">I hereby acknowledgement that I may become <span>unbelievably swolenormous</span> and accept all risks of becoming the local <span>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
     </div>
   )
 }
