62a54728986c7063214071b606f593e2f02c7e1a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 20:13:16 +0000

    Working on the section wrapper component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -3,7 +3,7 @@ import SectionWrapper from "./SectionWrapper";
 export default function Generator() {
   return (
     <SectionWrapper title={["It's", "Huge", "o'clock"]} header={"generate your workout"}>
-
+        hildere
     </SectionWrapper>
   )
 }
