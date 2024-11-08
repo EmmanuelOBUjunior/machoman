a38d4dc827f59ebc80ecc3454d363b105588652f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 20:57:50 +0000

    Working on a header component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -6,7 +6,7 @@ function Header({index, title, description}){
     <div className="flex flex-col gap-4">
       <div className="flex items-center  justify-center gap-2">
         <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">{index}</p>
-        <h4 className="text-lg sm:text-2xl md:text-3xl">{title}</h4>
+        <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
       </div>
       <p className="text-sm sm:text-base mx-auto">{description}</p>
     </div>
