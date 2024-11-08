943a2bf38495dd1011d6de09aa1fee61842cb71d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 21:06:38 +0000

    Mapping and rendering workout types

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,31 +1,42 @@
 import { WORKOUTS } from "../utils/swoldier";
 import SectionWrapper from "./SectionWrapper";
 
-
-function Header({index, title, description}){
-  return(
+function Header({ index, title, description }) {
+  return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center  justify-center gap-2">
-        <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">{index}</p>
+        <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">
+          {index}
+        </p>
         <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
       </div>
       <p className="text-sm sm:text-base mx-auto">{description}</p>
     </div>
-  )
+  );
 }
 export default function Generator() {
   return (
-    <SectionWrapper title={["It's", "Huge", "o'clock"]} header={"generate your workout"}>
-        <Header index={"01"} title={"Pick your poison"} description={"Select the workout you wish to endure"}/>
-        <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
-        {Object.keys(WORKOUTS).map((type, typeIndex)=>{
-          return(
-            <button key={typeIndex} className="bg-slate-950 border-blue-400 border py-3 rounded-lg">
+    <SectionWrapper
+      title={["It's", "Huge", "o'clock"]}
+      header={"generate your workout"}
+    >
+      <Header
+        index={"01"}
+        title={"Pick your poison"}
+        description={"Select the workout you wish to endure"}
+      />
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        {Object.keys(WORKOUTS).map((type, typeIndex) => {
+          return (
+            <button
+              key={typeIndex}
+              className="bg-slate-950 border-blue-400 border py-3 rounded-lg"
+            >
               <p>{type}</p>
             </button>
-          )
+          );
         })}
-        </div>
+      </div>
     </SectionWrapper>
-  )
+  );
 }
