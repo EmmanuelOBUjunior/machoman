a47a05dc456e2e41bdd8c48c496eae932a55a8a2
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 20:20:49 +0000

    Working on the section wrapper component

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,7 +1,9 @@
 
 function SectionWrapper({children, title, header}) {
   return (
-    <section>{children}</section>
+    <section className="min-h-screen flex flex-col gap-10">
+        <div className="bg-slate-950 py-10 flex flex-col gap-4 justify-center items-center"></div>
+    </section>
   )
 }
 
