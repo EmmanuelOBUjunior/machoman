33d339dad9dd3c53bc13b8e7ffa575325aa2eb72
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 20:47:33 +0000

    Working on a header component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,5 +1,15 @@
 import SectionWrapper from "./SectionWrapper";
 
+
+function Header({index, title, description}){
+  return(
+    <div>
+      <div>
+        <p></p>
+      </div>
+    </div>
+  )
+}
 export default function Generator() {
   return (
     <SectionWrapper title={["It's", "Huge", "o'clock"]} header={"generate your workout"}>
