f2075ad832df6b42ec4ee7026a9aa07836fd9a74
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 20:57:24 +0000

    Working on a header component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -4,7 +4,7 @@ import SectionWrapper from "./SectionWrapper";
 function Header({index, title, description}){
   return(
     <div className="flex flex-col gap-4">
-      <div className="flex items-center gap-2">
+      <div className="flex items-center  justify-center gap-2">
         <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">{index}</p>
         <h4 className="text-lg sm:text-2xl md:text-3xl">{title}</h4>
       </div>
