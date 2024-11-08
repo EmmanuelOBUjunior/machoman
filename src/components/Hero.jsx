85d59efe1b231826d7cab46678979d1fcb72200f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 20:04:07 +0000

    working on the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 
 export default function Hero() {
   return (
-    <div className="min-h-screen gap-10 flex flex-col items-center justify-center text-center max-w-[800px] w-full mx-auto">
+    <div className="min-h-screen gap-10 flex flex-col items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
       <div className="flex flex-col gap-4">
       <p>IT&apos;S TIME TO BE A</p>
       <h1 className="uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Macho<span className="text-blue-400">Man</span></h1>
