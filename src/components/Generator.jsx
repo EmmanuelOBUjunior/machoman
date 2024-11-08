409b415393661c08d6e2e78de09adc210656e50a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 21:45:52 +0000

    Working on the second header

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -39,6 +39,23 @@ export default function Generator() {
           );
         })}
       </div>
+      <Header
+        index={"01"}
+        title={"Pick your poison"}
+        description={"Select the workout you wish to endure"}
+      />
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        {Object.keys(WORKOUTS).map((type, typeIndex) => {
+          return (
+            <button
+              key={typeIndex}
+              className="bg-slate-950 border-blue-400 border py-3 rounded-lg duration-200 hover:border-blue-600"
+            >
+              <p className="capitalize">{type.replaceAll("_", " ")}</p>
+            </button>
+          );
+        })}
+      </div>
     </SectionWrapper>
   );
 }
