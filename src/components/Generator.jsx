0532171ca2fdc1d2202cb763d16d62c25c84a1f6
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 20:48:14 +0000

    Working on a header component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -5,15 +5,17 @@ function Header({index, title, description}){
   return(
     <div>
       <div>
-        <p></p>
+        <p>{index}</p>
+        <h4>{title}</h4>
       </div>
+      <p>{description}</p>
     </div>
   )
 }
 export default function Generator() {
   return (
     <SectionWrapper title={["It's", "Huge", "o'clock"]} header={"generate your workout"}>
-        hildere
+        <Header/>
     </SectionWrapper>
   )
 }
