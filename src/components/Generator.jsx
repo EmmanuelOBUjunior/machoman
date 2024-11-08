522a823a69eedaa054c1eb585d7c6aa31d1c8f81
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 21:38:00 +0000

    Mapping and rendering workout types

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -32,7 +32,7 @@ export default function Generator() {
           return (
             <button
               key={typeIndex}
-              className="bg-slate-950 border-blue-400 border py-3 rounded-lg"
+              className="bg-slate-950 border-blue-400 border py-3 rounded-lg duration-200 hover:border-blue-600"
             >
               <p>{type}</p>
             </button>
