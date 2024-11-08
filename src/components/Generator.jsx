7ea7296dbc93b2f298ca9c565fca5901d4845a77
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-08 21:12:36 +0000

    Added prop types dependency

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -43,7 +43,7 @@ export default function Generator() {
   );
 }
 
-const myPropTypes = {
+Header.PropTypes = {
   index: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
