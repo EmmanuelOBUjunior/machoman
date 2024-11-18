397f29cc3db10008105f7ca11cb92b5d875749b0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-18 08:42:29 +0000

    working on the generator section

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -47,7 +47,7 @@ export default function Generator() {
         description={"Select the muscles judged for annihilation."}
       />
       <div className="bg-slate-950 p-3 border border-solid border-blue-400 rounded-lg">
-        <button className="flex items-center relative justify-center">
+        <button className="relative flex items-center p-3 relative justify-center">
           <p>Select muscle groups</p>
           <i className="fa-solid fa-caret-down absolute right-3 top-1/2 -translate-y-1/2"></i>
         </button>
