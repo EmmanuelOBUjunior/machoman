eaa10a2730ab5f3f1dddd6a6b98cc37c8d9e62f3
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-11-18 09:15:14 +0000

    working on generator

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -39,7 +39,7 @@ export default function Generator() {
             <button
               onClick={() => setPoision(type)}
               key={typeIndex}
-              className={"bg-slate-950 border py-3 rounded-lg duration-200 hover:border-blue-600 " + (type === poision? ' border-3 border-blue-600':' border-blue-400')}
+              className={'bg-slate-950 border py-3 rounded-lg duration-200 hover:border-blue-600 ' + (type === poision? ' border-[3px] border-blue-600':' border-blue-400')}
             >
               <p className="capitalize">{type.replaceAll("_", " ")}</p>
             </button>
