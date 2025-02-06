a51ffac4d9c1bd458e80a23ccebe588725301fbf
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-06 08:00:16 +0000

    chore:validating prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,13 @@
-
-const Button = () => {
+import PropTypes from "prop-types"
+const Button = ({text}) => {
   return (
-    <button className="px-8 py-4 rounded-md border-2 border-solid border-blue-400 bg-slate-950 blueShadow duration-200"><p>Accept & Begin</p></button>
+    <button className="px-8 py-4 rounded-md border-2 border-solid border-blue-400 bg-slate-950 blueShadow duration-200"><p>{text}</p></button>
   )
 }
 
 export default Button
+
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+}
