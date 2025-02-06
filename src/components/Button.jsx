03c6b23b82284bd0dc71ee05d48c9b0d64f5f27c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-06 07:58:09 +0000

    feat: working on the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 
 const Button = () => {
   return (
-    <div>Button</div>
+    <button className="px-8 py-4 rounded-md border-2 border-solid border-blue-400 bg-slate-950 blueShadow duration-200"><p>Accept & Begin</p></button>
   )
 }
 
